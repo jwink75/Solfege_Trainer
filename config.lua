e9d52667ac9bb8101c93a1b7a9c63e2cb17e0fb7
Jacob Winkler <50600515+jwink75@users.noreply.github.com>
2026-08-01 13:51:03 -0700

    Optimize build.settings and config.lua for mobile device deployment (iOS/Android landscape locking, status bar hiding, and retina scaling)

--- config.lua
+++ config.lua
@@ -4,5 +4,9 @@ application = {
         height = 320,
         scale = "letterbox",
         fps = 60,
+        imageSuffix = {
+            ["@2x"] = 1.5,
+            ["@3x"] = 2.5,
+        },
     },
 }
